1313fafe0d93dff4135c8e48e6367ebe7b8778a4
Vibudh Mishra <vibudhm@gmail.com>
2015-06-23 09:44:03 -0500

    JS Code

--- js/app.js
+++ js/app.js
@@ -1,22 +1,35 @@
 
-$(document).ready(function() {
-  $('.ryu').mouseenter(function() {
-    $('.ryu-still').hide();
-	$('.ryu-ready').show();
-  });
-  $('.ryu').mouseleave(function() {
-    $('.ryu-ready').hide();
-	$('.ryu-still').show();
-  });
-   $('.ryu').mousedown(function() {
-    $('.ryu-ready').hide();
-    $('.ryu-throwing').show();
-    $('.hadouken').show();
-    
-  })
-   $('.ryu').mouseup(function() {
-    $('.ryu-throwing').hide();
-  	$('.ryu-ready').show();
-    
-  });
+$(document).ready(function(){
+    $('.ryu').mouseenter(function(){
+        $('.ryu-still').hide();
+        $('.ryu-ready').show();
+    })
+    .mouseleave(function() {
+        $('.ryu-still').show();
+        $('.ryu-ready').hide();
+    })
+    $('.ryu').mousedown(function(){
+        
+        $('.ryu-ready').hide();
+        $('.ryu-throwing').show();
+        $('.hadouken').finish().show().animate(
+            {'left': '1020px'},
+            500,
+            function(){
+                $(this).hide();
+                $(this).css('left', '520px');
+            });
+        playHadouken();
+    })
+    $('.ryu').mouseup(function(){
+        $('.ryu-throwing').hide();
+        $('.ryu-ready').show();
+    });
+
 });
+
+function playHadouken() {
+        $('#hadouken-sound')[0].volume = 0.5;
+        $('#hadouken-sound')[0].load();
+        $('#hadouken-sound')[0].play();
+    }
